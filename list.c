59a4ed1b35fb6a539e5f71051c6c41a1d44be493
emz1003 <emzhang1003@gmail.com>
2019-10-26 20:07:44 -0400

    test free_list

--- list.c
+++ list.c
@@ -4,25 +4,50 @@
 
 void print_list(struct node * n) {
     printf("[");
-    struct node * m = n;
-    while(m) {
-        printf("%i", m->i);
-        m = m->next;
-        if(m) {
+    while(n) {
+        printf("%i", n->i);
+        n = n->next;
+        if(n) {
             printf(", ");
+        } else {
+            break;
         }
     }
     printf("]\n");
 }
 
-// struct node *insert_front(struct node * n, int i) {
+struct node *insert_front(struct node * n, int i) {
+    struct node * new;
+    new->i = i;
+    new->next = n;
+    return new;
+}
 
-// }
+struct node *free_list(struct node * n) {
+    printf("freeing %d\n", n->i);
+    if (!n->next) {
+        free(n);
+        return NULL;
+    }
+    return free_list(n->next);
+}
 
-// struct node *free_list(struct node * n) {
+struct node *remove_node(struct node *front, int data) {
+    struct node * temp = front;
+    if(front->i == data) {
+        struct node * t = front->next;
+        free(front);
+        return t;
+    }
 
-// }
-
-// struct node *remove_node(struct node *front, int data) {
-
-// }
+    while(temp) {
+        if (temp->next->i == data) {
+            struct node * t = temp->next;
+            temp->next = t->next;
+            free(t);
+            break;
+        }
+        temp = temp->next;
+    }
+    return front;
+}
--- main.c
+++ main.c
@@ -3,17 +3,57 @@
 #include "list.h"
 
 int main() {
-    void *p = malloc(sizeof(struct node));
+    void *p = calloc(1, sizeof(struct node));
     struct node * list = p;
-    list->i = 0;
-    // int n;
-    // for (n = 0; n < 10; n++) {
-    //     struct node *another = malloc(sizeof(struct node));
-    //     list->i = n;
-    //     list->next = another;
-    //     list = list->next;
-    // }
+    struct node * temp = p;
+
+    int n;
+    for (n = 0; n < 10; n++) {
+        printf("adding %d to list\n", n);
+        struct node *another = calloc(1, sizeof(struct node));
+        temp->i = n;
+        if(n != 9){
+            temp->next = another;
+            temp = temp->next;
+        }
+    }
+    printf("printlist(list): ");
     print_list(list);
-    free(list);
+
+    printf("insert_front(list, 100): ");
+    list = insert_front(list, 100);
+    print_list(list);
+
+    printf("freelist(list):");
+    list = free_list(list);
+    print_list(list);
+
+    void *rm = calloc(1, sizeof(struct node));
+    struct node *anotherList = rm;
+    struct node *tempPointer = rm;
+
+    for (n = 0; n < 10; n++)
+    {
+        printf("adding %d to anotherList\n", n);
+        struct node *another = calloc(1, sizeof(struct node));
+        tempPointer->i = n;
+        if (n != 9)
+        {
+            tempPointer->next = another;
+            tempPointer = tempPointer->next;
+        }
+    }
+
+    printf("remove_node(anotherList, 4): ");
+    anotherList = remove_node(anotherList, 4);
+    print_list(anotherList);
+
+    if(!p){
+        free(p);
+    }
+    
+    if(!rm) {
+        free(rm);
+    }
     return 0;
 }
